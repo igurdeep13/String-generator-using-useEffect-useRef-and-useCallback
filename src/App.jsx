28d4d7219b6b26fa12894cee0f92c42f51b25d9c
igurdeep13 <g.s.basantpuria@gmail.com>
2024-10-05 13:58:31 -0400

    Add tailwind classes in component, password generator function and so on

--- src/App.jsx
+++ src/App.jsx
@@ -12,12 +12,29 @@ function App() {
     let str = "ABCDEFGHIJKLMNOPQRSTUVWXZYabcdefghijklmnopqrstuvwxyz";
 
     if (numberAllowed) str += "0123456789";
-    if (charAllowed) str += "!@#$%^&*-_+=[]{}~`"
+    if (charAllowed) str += "!@#$%^&*-_+=[]{}~`";
 
-  }, [length, numberAllowed, charAllowed]);
+    for (let i = 1; i <= array.length; i++) {
+      let char = Math.floor(Math.random() * str.length + 1);
+
+      pass = str.charAt(char);
+    }
+    setPassword(pass);
+  }, [length, numberAllowed, charAllowed, setPassword]);
   return (
     <>
-      <h1 className="text-4xl text-center text-white">Password generator</h1>
+      <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 text-orange-500 bg-gray-800">
+      <h1 className="text-white text-center">Password Generator</h1>
+        <div className="flex shadow rounded-lg overflow-hidden mb-4">
+          <input
+            type="text"
+            value={password}
+            className="outline-none w-full py-1 px-3"
+            placeholder="password"
+            readOnly
+          />
+        </div>
+      </div>
     </>
   );
 }
